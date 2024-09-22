2c79d467b23d4a4c812935b2cba909484ada468b
James Atkins <james@jamesatkins.com>
2024-09-22 15:24:33 -0500

    Add rlang purrr standalone

--- R/statable-package.R
+++ R/statable-package.R
@@ -4,8 +4,10 @@
 ## usethis namespace: start
 #' @importFrom cli cli_abort
 #' @importFrom cli cli_inform
+#' @importFrom rlang as_function
 #' @importFrom rlang check_dots_empty
 #' @importFrom rlang env_get
 #' @importFrom rlang env_has
+#' @importFrom rlang set_names
 ## usethis namespace: end
 NULL
