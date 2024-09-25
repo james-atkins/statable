518f8468ec3ad64b729e7fd92121e884b2844b19
James Atkins <james@jamesatkins.com>
2024-09-24 19:21:42 -0500

    Suppress Stata output when running tests

--- tests/testthat/test-stata.R
+++ tests/testthat/test-stata.R
@@ -1,13 +1,15 @@
 test_that("stata works", {
   skip_if_no_stata()
 
-  session <- stata_start_session()
-  on.exit(stata_close_session(session))
+  suppressMessages({
+    session <- stata_start_session()
+    on.exit(stata_close_session(session))
 
-  stata_run("sysuse auto", session = session)
-  auto <- stata_data_out(session = session)
+    stata_run("sysuse auto", session = session)
+    auto <- stata_data_out(session = session)
 
-  expect_equal(mean(auto$price), 6165.2568)
+    expect_equal(mean(auto$price), 6165.2568)
 
-  expect_error(stata_run("this_is_an_invalid_command, oops", session = session), class = "statable_error_stata")
+    expect_error(stata_run("this_is_an_invalid_command, oops", session = session), class = "statable_error_stata")
+  })
 })
